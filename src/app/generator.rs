67c241d3ec2488857e37d4071bdf9a5d74a289cd
Michael Furlong <breakingshards@gmail.com>
2024-02-03 05:56:47 +0000

    Multi-threaded file generation when there are multiple files.

--- src/app/generator.rs
+++ src/app/generator.rs
@@ -16,9 +16,9 @@
 //
 use chrono::{Datelike, Month, Timelike, Utc};
 use rlua::Lua;
-use std::{fs, io::Write, path::PathBuf};
+use std::{fs, io::Write, path::PathBuf, thread::{self, JoinHandle}};
 
-use crate::{app::*, box_error, name::*, paths::*, SMResult};
+use crate::{app::*, box_error, make_error, name::*, paths::*, SMError, SMResult};
 
 pub fn generate_file(appdata: &AppData) -> SMResult<()>
 {
@@ -83,330 +83,349 @@ pub fn generate_file(appdata: &AppData) -> SMResult<()>
 		tfl
 	};
 
-	for tf in &tfiles
+	let mut ft: Vec<JoinHandle<Result<(), SMError>>> = Vec::new();
+
+	for tf in tfiles
 	{
-		// Open template file and read in to string.
-		let mut content = match fs::read_to_string(&tf)
-		{
-			Ok(con) => con,
-			Err(e) =>
+		let ad = appdata.clone();
+
+		ft.push( thread::spawn( move || {
+			// Open template file and read in to string.
+			let mut content = match fs::read_to_string(&tf)
 			{
-				return Err(box_error(&format!(
-					"Unable to read template from file {tf} because: {e}"
-				)))
-			}
-		};
-
-		let fileext = get_extention(&tf);
-
-		if !appdata.languages[appdata.language]
-			.script_path
-			.trim()
-			.is_empty()
-		{
-			// Get language script and ensure it exists.
-			let scriptdata = {
-				let mut scriptpath = scripts_dir();
-
-				scriptpath += &appdata.languages[appdata.language].script_path;
-
-				match fs::read_to_string(&scriptpath)
+				Ok(con) => con,
+				Err(e) =>
 				{
-					Ok(con) => con,
-					Err(e) =>
-					{
-						return Err(box_error(&format!(
-							"Unable to read language script {scriptpath} because: {e}"
-						)))
-					}
+					return Err(make_error(&format!(
+						"Unable to read template from file {tf} because: {e}"
+					)))
 				}
 			};
 
-			// Start lua instance.
-			let lua = Lua::new();
+			let fileext = get_extention(&tf);
 
-			match lua.context(|lua_ctx| {
-				let luaargs = {
-					let la = match lua_ctx.create_table()
+			if !ad.languages[ad.language]
+				.script_path
+				.trim()
+				.is_empty()
+			{
+				// Get language script and ensure it exists.
+				let scriptdata = {
+					let mut scriptpath = scripts_dir();
+
+					scriptpath += &ad.languages[ad.language].script_path;
+
+					match fs::read_to_string(&scriptpath)
 					{
-						Ok(t) => t,
+						Ok(con) => con,
 						Err(e) =>
 						{
-							return Err(format!("Failed creating lua table for SMArguments: {e}"))
+							return Err(make_error(&format!(
+								"Unable to read language script {scriptpath} because: {e}"
+							)))
 						}
-					};
-
-					let mut ai = 1;
-
-					for a in &appdata.args
-					{
-						if la.set(ai, a.clone()).is_err()
-						{
-							return Err(format!("Failed setting lua language script argument."));
-						}
-
-						ai += 1;
 					}
-
-					la
 				};
 
-				let globals = lua_ctx.globals();
+				// Start lua instance.
+				let lua = Lua::new();
 
-				// Define variables in lua that the scripts use.
-				if globals
-					.set("SMFileName", get_file_name(&tf, false))
-					.is_err()
-				{
-					return Err(format!("Failed initialising lua variable SMFileName."));
-				}
-				if globals
-					.set("SMSafeName", path_to_name(&appdata.name, '_'))
-					.is_err()
-				{
-					return Err(format!("Failed initialising lua variable SMSafeName."));
-				}
-				if globals.set("SMArguments", luaargs).is_err()
-				{
-					return Err(format!("Failed setting lua language script arguments."));
-				}
-
-				// Load the script into lua.
-				match lua_ctx.load(&scriptdata).exec()
-				{
-					Ok(_) =>
-					{}
-					Err(e) => return Err(format!("Failed parsing language script: {e}.")),
-				}
-				// Ensure script has required functionsand call ProcessArguments function from lua.
-				match lua_ctx.load("ReplaceMacro ~= nil").eval::<bool>()
-				{
-					Ok(f) =>
-					{
-						if !f
+				match lua.context(|lua_ctx| {
+					let luaargs = {
+						let la = match lua_ctx.create_table()
 						{
-							return Err(format!("Language script missing ReplaceMacro function."));
-						}
-					}
-					Err(e) =>
-					{
-						return Err(format!("Failed ReplaceMacro check in language script: {e}"));
-					}
-				}
-				match lua_ctx.load("ProcessArguments ~= nil").eval::<bool>()
-				{
-					Ok(f) =>
-					{
-						if f && lua_ctx.load("ProcessArguments()").exec().is_err()
-						{
-							return Err(format!(
-								"Failed running ProcessArguments() in language script."
-							));
-						}
-					}
-					Err(e) =>
-					{
-						return Err(format!(
-							"Failed ProcessArguments check in language script: {e}"
-						))
-					}
-				}
-
-				// Parse string, calling the language scripts' ReplaceMacro function from lua to replace macros.
-				loop
-				{
-					let mut mac = content.find('$');
-
-					if mac.is_none()
-					{
-						break;
-					}
-
-					let mut replaced = false;
-
-					while mac.is_some() && mac.unwrap() < content.len()
-					{
-						let begin = mac.unwrap();
-						let end = match content[begin + 1..].find('$')
-						{
-							Some(e) => e + begin + 1,
-							_ => break,
-						};
-
-						let macstr = String::from(&content[begin..end + 1].to_uppercase());
-
-						if !is_valid_name(&macstr[1..macstr.len() - 1])
-						{
-							continue;
-						}
-
-						let repl = match lua_ctx
-							.load(&format!("ReplaceMacro(\"{}\")", &macstr))
-							.eval::<String>()
-						{
-							Ok(f) => f,
+							Ok(t) => t,
 							Err(e) =>
 							{
-								return Err(format!(
-									"Failed running ReplaceMacro in language script: {e}"
-								))
+								return Err(format!("Failed creating lua table for SMArguments: {e}"))
 							}
 						};
 
-						if macstr != repl.to_uppercase()
+						let mut ai = 1;
+
+						for a in &ad.args
 						{
-							content = content.replace(&macstr, &repl);
-							replaced = true;
+							if la.set(ai, a.clone()).is_err()
+							{
+								return Err(format!("Failed setting lua language script argument."));
+							}
+
+							ai += 1;
 						}
 
-						if repl.is_empty()
+						la
+					};
+
+					let globals = lua_ctx.globals();
+
+					// Define variables in lua that the scripts use.
+					if globals
+						.set("SMFileName", get_file_name(&tf, false))
+						.is_err()
+					{
+						return Err(format!("Failed initialising lua variable SMFileName."));
+					}
+					if globals
+						.set("SMSafeName", path_to_name(&ad.name, '_'))
+						.is_err()
+					{
+						return Err(format!("Failed initialising lua variable SMSafeName."));
+					}
+					if globals.set("SMArguments", luaargs).is_err()
+					{
+						return Err(format!("Failed setting lua language script arguments."));
+					}
+
+					// Load the script into lua.
+					match lua_ctx.load(&scriptdata).exec()
+					{
+						Ok(_) =>
+						{}
+						Err(e) => return Err(format!("Failed parsing language script: {e}.")),
+					}
+					// Ensure script has required functionsand call ProcessArguments function from lua.
+					match lua_ctx.load("ReplaceMacro ~= nil").eval::<bool>()
+					{
+						Ok(f) =>
 						{
-							if begin == 0
+							if !f
 							{
-								if &content[..1] == " "
+								return Err(format!("Language script missing ReplaceMacro function."));
+							}
+						}
+						Err(e) =>
+						{
+							return Err(format!("Failed ReplaceMacro check in language script: {e}"));
+						}
+					}
+					match lua_ctx.load("ProcessArguments ~= nil").eval::<bool>()
+					{
+						Ok(f) =>
+						{
+							if f && lua_ctx.load("ProcessArguments()").exec().is_err()
+							{
+								return Err(format!(
+									"Failed running ProcessArguments() in language script."
+								));
+							}
+						}
+						Err(e) =>
+						{
+							return Err(format!(
+								"Failed ProcessArguments check in language script: {e}"
+							))
+						}
+					}
+
+					// Parse string, calling the language scripts' ReplaceMacro function from lua to replace macros.
+					loop
+					{
+						let mut mac = content.find('$');
+
+						if mac.is_none()
+						{
+							break;
+						}
+
+						let mut replaced = false;
+
+						while mac.is_some() && mac.unwrap() < content.len()
+						{
+							let begin = mac.unwrap();
+							let end = match content[begin + 1..].find('$')
+							{
+								Some(e) => e + begin + 1,
+								_ => break,
+							};
+
+							let macstr = String::from(&content[begin..end + 1].to_uppercase());
+
+							if !is_valid_name(&macstr[1..macstr.len() - 1])
+							{
+								continue;
+							}
+
+							let repl = match lua_ctx
+								.load(&format!("ReplaceMacro(\"{}\")", &macstr))
+								.eval::<String>()
+							{
+								Ok(f) => f,
+								Err(e) =>
+								{
+									return Err(format!(
+										"Failed running ReplaceMacro in language script: {e}"
+									))
+								}
+							};
+
+							if macstr != repl.to_uppercase()
+							{
+								content = content.replace(&macstr, &repl);
+								replaced = true;
+							}
+
+							if repl.is_empty()
+							{
+								if begin == 0
+								{
+									if &content[..1] == " "
+									{
+										content.remove(begin);
+									}
+								}
+								else if &content[begin - 1..begin + 1] == "  "
 								{
 									content.remove(begin);
 								}
 							}
-							else if &content[begin - 1..begin + 1] == "  "
+
+							mac = content[begin + repl.len()..].find('$');
+
+							if mac.is_some()
 							{
-								content.remove(begin);
+								mac = Some(mac.unwrap() + begin + repl.len());
 							}
 						}
 
-						mac = content[begin + repl.len()..].find('$');
-
-						if mac.is_some()
+						if !replaced
 						{
-							mac = Some(mac.unwrap() + begin + repl.len());
+							break;
 						}
 					}
 
-					if !replaced
-					{
-						break;
-					}
+					return Ok(());
+				})
+				{
+					Err(e) => return Err(make_error(&format!("Lua context fail: {e}"))),
+					Ok(_) =>
+					{}
+				}
+			}
+
+			// Replace built-in macros (do this after the language to allow its script to override default behaviour).
+			content = replace_universal_macros(&ad, content, fileext);
+			// Cleanup whitespace.
+			let mut nlns = content.find("\n\n\n").is_some() || content.find("\r\n\r\n\r\n").is_some();
+
+			while nlns
+			{
+				content = content
+					.replace("\r\n\r\n\r\n", "\r\n\r\n")
+					.replace("\n\n\n", "\n\n");
+				nlns = content.find("\n\n\n").is_some() || content.find("\r\n\r\n\r\n").is_some();
+			}
+
+			let mut targetpath = PathBuf::new();
+			targetpath.push(&ad.directory);
+
+			let dataname = ad.name.replace("\\", "/");
+			let split: Vec<&str> = dataname.split("/").collect();
+
+			if !split.len() > 1
+			{
+				for s in split
+				{
+					targetpath.push(s);
 				}
 
-				return Ok(());
-			})
-			{
-				Err(e) => return Err(box_error(&format!("Lua context fail: {e}"))),
-				Ok(_) =>
-				{}
-			}
-		}
-
-		// Replace built-in macros (do this after the language to allow its script to override default behaviour).
-		content = replace_universal_macros(appdata, content, fileext);
-		// Cleanup whitespace.
-		let mut nlns = content.find("\n\n\n").is_some() || content.find("\r\n\r\n\r\n").is_some();
-
-		while nlns
-		{
-			content = content
-				.replace("\r\n\r\n\r\n", "\r\n\r\n")
-				.replace("\n\n\n", "\n\n");
-			nlns = content.find("\n\n\n").is_some() || content.find("\r\n\r\n\r\n").is_some();
-		}
-
-		let mut targetpath = PathBuf::new();
-		targetpath.push(&appdata.directory);
-
-		let dataname = appdata.name.replace("\\", "/");
-		let split: Vec<&str> = dataname.split("/").collect();
-
-		if !split.len() > 1
-		{
-			for s in split
-			{
-				targetpath.push(s);
+				targetpath.pop();
 			}
 
-			targetpath.pop();
-		}
+			_ = fs::create_dir_all(&targetpath);
 
-		_ = fs::create_dir_all(&targetpath);
+			let fname = get_file_name(&ad.name, true);
+			targetpath.push(fname + "." + fileext);
 
-		let fname = get_file_name(&appdata.name, true);
-		targetpath.push(fname + "." + fileext);
-
-		let exists = match fs::try_exists(&targetpath)
-		{
-			Ok(e) => e,
-			_ => false,
-		};
-
-		if exists && appdata.overwrite.is_none()
-		{
-			println!(
-				"A file already exists at {}. Would you like to overwrite it? (Y/N)",
-				targetpath.display()
-			);
-
-			let mut line = String::new();
-
-			if std::io::stdin().read_line(&mut line).is_err()
+			let exists = match fs::try_exists(&targetpath)
 			{
-				continue;
-			}
+				Ok(e) => e,
+				_ => false,
+			};
 
-			let answer = line.trim().to_lowercase();
-
-			if answer != "y" && answer != "yes"
-			{
-				continue;
-			}
-		}
-		else if exists && appdata.overwrite.is_some()
-		{
-			let ov = appdata.overwrite.unwrap();
-
-			if !ov
+			if exists && ad.overwrite.is_none()
 			{
 				println!(
-					"A file already exists at {}. It will not be overwritten.",
+					"A file already exists at {}. Would you like to overwrite it? (Y/N)",
 					targetpath.display()
 				);
 
-				continue;
-			}
-			else
-			{
-				println!(
-					"A file already exists at {}. It will be overwritten.",
-					targetpath.display()
-				);
-			}
-		}
+				let mut line = String::new();
 
-		match fs::File::create(&targetpath)
-		{
-			Ok(mut file) => match file.write_all(content.as_bytes())
+				if std::io::stdin().read_line(&mut line).is_err()
+				{
+					return Ok(());
+				}
+
+				let answer = line.trim().to_lowercase();
+
+				if answer != "y" && answer != "yes"
+				{
+					return Ok(());
+				}
+			}
+			else if exists && ad.overwrite.is_some()
 			{
-				Ok(()) =>
-				{}
+				let ov = ad.overwrite.unwrap();
+
+				if !ov
+				{
+					println!(
+						"A file already exists at {}. It will not be overwritten.",
+						targetpath.display()
+					);
+
+					return Ok(());
+				}
+				else
+				{
+					println!(
+						"A file already exists at {}. It will be overwritten.",
+						targetpath.display()
+					);
+				}
+			}
+
+			match fs::File::create(&targetpath)
+			{
+				Ok(mut file) => match file.write_all(content.as_bytes())
+				{
+					Ok(()) =>
+					{}
+					Err(e) =>
+					{
+						return Err(make_error(&format!(
+							"Unable to write file {}: {e}",
+							targetpath.display()
+						)))
+					}
+				},
 				Err(e) =>
 				{
-					return Err(box_error(&format!(
-						"Unable to write file {}: {e}",
+					return Err(make_error(&format!(
+						"Unable to create file {}: {e}",
 						targetpath.display()
 					)))
 				}
-			},
-			Err(e) =>
-			{
-				return Err(box_error(&format!(
-					"Unable to create file {}: {e}",
-					targetpath.display()
-				)))
+			};
+
+			println!(
+				"Srcmake created the file: {} successfully.",
+				targetpath.display()
+			);
+
+			Ok(())
+		}));
+	}
+
+	for t in ft
+	{
+		match t.join()
+		{
+			Ok(_) => {},
+			Err(_) => {
+				return Err(box_error("Failed generating file."));
 			}
 		};
-
-		println!(
-			"Srcmake created the file: {} successfully.",
-			targetpath.display()
-		);
 	}
 
 	Ok(())
